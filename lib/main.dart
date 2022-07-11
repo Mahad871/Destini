a6b7557c3babdc946f268ab5728006d026fbb112
Mahad Saleem <ms.asd871@gmail.com>
2022-07-12 02:32:54 +0500

    starting

--- lib/main.dart
+++ lib/main.dart
@@ -25,13 +25,14 @@ class _StoryPageState extends State<StoryPage> {
     return Scaffold(
       body: Container(
         //TODO: Step 1 - Add background.png to this Container as a background image.
-        
+
         padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
         constraints: BoxConstraints.expand(),
         child: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
+              Image.asset("assets/images/"),
               Expanded(
                 flex: 12,
                 child: Center(
